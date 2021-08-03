a188d9a5900bbd9ed545a8582b03cc0fd912caa4
ekpotosammuel <62830104+ekpotosammuel@users.noreply.github.com>
2021-08-03 07:16:37 -0700

    email and phone uniqueness

--- Sceneventory/Waitlist/models.py
+++ Sceneventory/Waitlist/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class UserInfo(models.Model):
     name = models.CharField(max_length=150)
-    email = models.EmailField(max_length=100)
-    phone = models.IntegerField(max_length=11)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.IntegerField(max_length=11, unique=True)
     traffic = models.CharField(max_length=150)
     date = models.DateTimeField(auto_now_add=True)
 
